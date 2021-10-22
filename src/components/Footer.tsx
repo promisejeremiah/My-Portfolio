5aabb281b0c77ce4cd043d16c4cfee99bd0c474b
Promise Jeremiah <48945500+promisejeremiah@users.noreply.github.com>
2021-10-22 09:59:43 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     const yearNow = new Date().getFullYear();
     return (
-        <footer className="container-fluid mt-5">
-            <div className="footer text-center pt-4">
+        <footer className="container-fluid">
+            <div className="footer text-center">
                 <span className="text-muted">made with ❤️ by JEREMIAH PROMISE ANIEDI &copy; 2021-{yearNow} </span>
                 <Link to={''}><i className="fa fa-twitter m-3"></i></Link>
                 <Link to={''}><i className="fa fa-linkedin m-3"></i></Link>
