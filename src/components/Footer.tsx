6e051a39aec6dd49e2a662f811708975c3733474
Promise Jeremiah <48945500+promisejeremiah@users.noreply.github.com>
2021-10-25 05:41:48 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <footer className="container-fluid">
             <div className="footer text-center">
                 <span className="text-muted">made with ❤️ by JEREMIAH PROMISE ANIEDI &copy; 2021-{yearNow} </span>
-                <Link to={'https://twitter.com/aniedipromisej'}><i className="fa fa-twitter m-3"></i></Link>
-                <Link to={'https://linkedin.com/in/promisejeremiah'}><i className="fa fa-linkedin m-3"></i></Link>
+                <a href="https://twitter.com/aniedipromisej"><i className="fa fa-twitter m-3"></i></a>
+                <a href="https://linkedin.com/in/promisejeremiah"><i className="fa fa-linkedin m-3"></i></a>
             </div>
         </footer>
     );
